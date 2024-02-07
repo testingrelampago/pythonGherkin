eaad71ba14de08acf17e107b5d4b899e4682270d
Yamil <currao.yamil@gmail.com>
2024-02-07 13:47:15 +0100

    Delete checkout.feature, I will use login and search example only

--- steps/search_steps.py
+++ steps/search_steps.py
@@ -1,21 +1,32 @@
+# -----
+
+#
+# Feature: Search functionality
+#
+
+# -----
+
 import os
-from behave import given, when, then
 from selenium import webdriver
 from dotenv import load_dotenv
+from behave import given, when, then
 from selenium.webdriver.chrome.options import Options
 
-# Cargar las variables de entorno desde el archivo .env
+# Add variable in .env
 load_dotenv()
 search_page_url = os.getenv("LOGIN_PAGE_URL")
 
+# -----
+
+# Scenario: User searches for a product
+
+# -----
+
 @given('the user is on the search page')
 def user_is_on_search_page(context):
-    # Crear opciones para el controlador Chrome
     chrome_options = Options()
-    # Opcionalmente, puedes configurar las opciones aquí, como:
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
 
-    # Inicializar el controlador de Chrome con las opciones
     context.custom_context.driver = webdriver.Chrome(options=chrome_options)
     context.custom_context.driver.get(search_page_url)
 
@@ -29,4 +40,18 @@ def search_results_display_relevant_products(context):
     # Implement verification code for relevant search results
     pass
 
-# Implement steps for other scenarios in search.feature
+# -----
+
+# Scenario: User searches with an empty query
+
+# -----
+
+@when('the user submits an empty search')
+def user_submits_empty_search(context):
+    # Implement code to enter a product name
+    pass
+
+@then('a message should indicate no results')
+def message_should_indicate_no_results(context):
+    # Implement verification code for relevant search results
+    pass
